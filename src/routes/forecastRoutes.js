0dc385051293b5e5cdd4ab6c518d423462e4b2fa
maxthemus <max.somerville62@gmail.com>
2023-10-16 21:45:29 +1300

    updates to endpoint

--- src/routes/forecastRoutes.js
+++ src/routes/forecastRoutes.js
@@ -2,9 +2,9 @@
 const express = require("express");
 const router = express.Router();
 
+const forecastModel = require("../controllers/forecastController");
 
-
-router.get("/data/all/allforecast", returnNotImplemented);
+router.get("/data/all/allforecast", forecastModel.getForecast);
 
 
 function returnNotImplemented(req, res) { 
